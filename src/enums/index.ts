80d04a09e35e338464fcaf30e9a522ee7cad4c09
Machynko <97360562+Machynko@users.noreply.github.com>
2022-05-01 17:10:20 +0200

    Update index.ts

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,11 +1,38 @@
 export enum Gender{
-    MALE = 'MALE',
-    FEMALE = 'FEMALE'
+  MALE = 'MALE',
+  FEMALE = 'FEMALE'
+}
+
+export enum PersonType {
+  CHILD = 'CHILD',
+  ADULT = 'ADULT'
+}
+
+export enum PatientWeight{
+  MAX = 200,
+  MIN = 1
+}
+
+export enum PatientHeight{
+  MAX = 250,
+  MIN = 10
+}
+
+export enum SubstanceAmount{
+  ADULT_MAX = 220,
+  CHILDREN_MAX = 120,
+  MIN = 20
 }
 
 export enum TimeUnit{
-    SECOND = 'SECOND',
-    MINUTE = 'MINUTE',
-    HOUR = 'HOUR',
-    DAY = 'DAY'
+  SECOND = 'SECOND',
+  MINUTE = 'MINUTE',
+  HOUR = 'HOUR',
+  DAY = 'DAY'
+}
+
+export enum UserRole{
+  USER = 'USER',
+  ADMIN = 'ADMIN',
+  SUPER_ADMIN = 'SUPER_ADMIN'
 }
