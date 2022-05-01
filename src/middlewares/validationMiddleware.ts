504ae281ee70c98a93156ffa222e551f2b681aca
Machynko <97360562+Machynko@users.noreply.github.com>
2022-05-01 17:10:41 +0200

    Update validationMiddleware.ts

--- src/middlewares/validationMiddleware.ts
+++ src/middlewares/validationMiddleware.ts
@@ -1,18 +1,19 @@
-import { Request, Response, NextFunction} from 'express'
-import Joi, {Schema} from "joi"
+import { Request, Response, NextFunction } from "express"
+import {Schema} from "joi"
+
 export default function validationMiddleware(schema: Schema) {
-    return (req: Request, res: Response, next: NextFunction) => {
-        const {query, body, params } = req
-        const validationResult = schema.validate({query, body, params})
+  return (req: Request, res: Response, next: NextFunction) => {
+    const { query, body, params } = req
+    const validationResult = schema.validate({ query, body, params})
 
-        if(validationResult.error){
-            return res.status(400).json(validationResult.error)
-        }
-
-        req.body = validationResult.value.body
-        req.params = validationResult.value.params
-        req.query = validationResult.value.query
-        
-        return next()
+    if (validationResult.error){
+      return res.status(400).json(validationResult.error)
     }
+
+    req.body = validationResult.value.body
+    req.params = validationResult.value.params
+    req.query = validationResult.value.query
+
+    return next()
+  }
 }
