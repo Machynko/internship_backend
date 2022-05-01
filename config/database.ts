d4205410869e5d1394f4a201c6e57bc10095fb01
Machynko <97360562+Machynko@users.noreply.github.com>
2022-05-01 17:11:58 +0200

    Update database.ts

--- config/database.ts
+++ config/database.ts
@@ -1,8 +1,29 @@
 import { Options } from 'sequelize'
 
-// eslint-disable-next-line import/prefer-default-export
 export const development = {
 	url: process.env.POSTGRESQL_URL,
+	options: <Options>{
+		minifyAliases: true,
+		logging: false,
+		pool: {
+			max: 4
+		}
+	}
+}
+
+export const production = {
+	url: process.env.POSTGRESQL_PROD_URL,
+	options: <Options>{
+		minifyAliases: true,
+		logging: false,
+		pool: {
+			max: 4
+		}
+	}
+}
+
+export const test = {
+	url: process.env.POSTGRESQL_TEST_URL,
 	options: <Options>{
 		minifyAliases: true,
 		logging: false,
