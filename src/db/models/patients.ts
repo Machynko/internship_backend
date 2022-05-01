f1e0267d3322d0fcabd33247a146b2416d96da34
Machynko <97360562+Machynko@users.noreply.github.com>
2022-05-01 17:08:25 +0200

    Update patients.ts

--- src/db/models/patients.ts
+++ src/db/models/patients.ts
@@ -1,78 +1,79 @@
-import { Sequelize, Model, DataTypes } from 'sequelize'
+import { DataTypes, Model, Sequelize } from "sequelize";
+import { Gender } from "../../enums/enums";
 import { Models } from "../index";
-import { Gender} from '../../enums'
-import { DiagnoseModel } from './diagnoses'
+import { DiagnoseModel } from "./diagnose_model";
 
 
 export class PatientModel extends Model {
+  id: number
+  firstName: string
+  lastName: string
+  birthdate: Date
+  weight: number
+  height: number
+  identificationNumber: string
+  gender: string
 
-    id: number
-    first_Name: string
-    last_Name: string
-    birth_date: Date
-    weight: number
-    height: number
-    gender: Gender
-    id_number: string
-    diagnose_id: number
-    diagnose: DiagnoseModel
-
+  diagnoseID: number
+  diagnose: DiagnoseModel
 }
 
 export default (sequelize: Sequelize, modelName: string) => {
-
-    PatientModel.init({
-        id: {
-            type: DataTypes.BIGINT,
-            allowNull: false,
-            primaryKey: true,
-            unique: true,
-            autoIncrement: true
-        },
-        first_Name: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        },
-        last_Name: {
-            type: DataTypes.TEXT,
-            allowNull: false
-        },
-        birth_date: {
-            type: DataTypes.DATEONLY,
-            allowNull: false
-        },
-        weight: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        height: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        id_number: {
-            type: DataTypes.STRING(12),
-            allowNull: false,
-            unique: true
-        },
-        gender: {
-            type: DataTypes.ENUM(Gender.MALE, Gender.FEMALE),
-            allowNull: false
-        },
-        diagnose_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        }
-    }, {
-        paranoid: false,
-        timestamps: false,
-        sequelize,
-        modelName,
-        tableName: 'patients'
-    });
-
-    (PatientModel as any).associate = (models: Models) => {
-        PatientModel.belongsTo(models.Diagnose, { foreignKey: 'diagnoseID'})
+  PatientModel.init(
+    {
+      id: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+        primaryKey: true,
+        unique: true,
+        autoIncrement: true,
+      },
+      firstName: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      birthdate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      weight: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      height: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      identificationNumber: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      gender: {
+        type: DataTypes.ENUM(Gender.MALE, Gender.FEMALE),
+        allowNull: false,
+      },
+      diagnoseID: {
+        type: DataTypes.BIGINT,
+        allowNull: false
+      }
+    },
+    {
+      paranoid: false,
+      timestamps: false,
+      sequelize,
+      modelName,
+      tableName: 'patients',
     }
+  );
 
-    return PatientModel
+  (PatientModel as any).associate = (models: Models) => {
+    PatientModel.belongsTo(models.Diagnose, { foreignKey: 'diagnoseID' })
+    PatientModel.hasOne(models.User, { foreignKey: 'patientID' })
+  }
+
+  return PatientModel
 }
