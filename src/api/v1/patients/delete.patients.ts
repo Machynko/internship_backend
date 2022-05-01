b6dc8c41a18985c559aca484ce8985aec91b8aea
Machynko <97360562+Machynko@users.noreply.github.com>
2022-05-01 17:05:20 +0200

    Update delete.patients.ts

--- src/api/v1/patients/delete.patients.ts
+++ src/api/v1/patients/delete.patients.ts
@@ -1,33 +1,40 @@
 import { Request, Response} from "express";
-const fs = require('fs');
-import { models } from "../../../db";
+import Joi from "joi";
+import { models} from "../../../db";
+import { Gender, PatientHeight, PatientWeight } from "../../../enums/enums";
 
+export const schema = Joi.object({
+  body: Joi.object({
+    firstName: Joi.string().max(100).required(),
+    lastName: Joi.string().max(100).required(),
+    birthdate: Joi.date().required(),
+    weight: Joi.number().integer().min(PatientWeight.MIN).max(PatientWeight.MAX).required(),
+    height: Joi.number().integer().min(PatientHeight.MIN).max(PatientHeight.MAX).required(),
+    identificationNumber: Joi.string().pattern(/^[a-zA-Z0-9]*$/).length(12).required(),
+    gender: Joi.string().valid(Gender.MALE, Gender.FEMALE).required(),
+    diagnoseID: Joi.number().integer().min(1).required()
+  }),
+  query: Joi.object(),
+  params: Joi.object()
+})
 
+export const workflow = async (req: Request, res: Response) => {
 
-export const workflow = (req: Request, res: Response) => {
-    JSON.parse(fs.readFileSync('./src/api/v1/patients/patients.json'));
-    const patientID: number = parseInt(req.params.patientID)
-    try {
-        models.Patient.destroy({where: {id: patientID}})
-        res.json({
-            "status": 200,
-            "messages": [
-                {
-                    "message": `Patient ${patientID} was deleted`,
-                    "type": "SUCCESS"
-                }
-            ]
-        })
-    } catch (e) {
-        res.json({
-            "status": 204,
-            "messages": [
-                {
-                    "details": e.errors,
-                    "type": "NO CONTENT"
-                }
-            ]
-        })
+  const {
+    Patient
+  } = models
+
+  const id = Number(req.params.id)
+
+  const patient = await Patient.destroy({
+    where: {
+      id
     }
+  })
 
+  if (!patient) {
+    res.status(404).json({ message: "Patient with this ID was not found", type: "FAILED" })
+  } else {
+    res.status(200).json({ message: `Patient with ID ${id} was succesfully deleted`, type: "SUCCESS" })
+  }
 }
