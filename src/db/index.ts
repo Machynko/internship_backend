4199949133c729789f2c192e877a3607642f6a49
Machynko <97360562+Machynko@users.noreply.github.com>
2022-05-02 19:31:00 +0200

    added defineuser

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,32 +1,39 @@
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
 import * as database from '../../config/database'
-import defineSubstance from './models/substance'
-import defineDiagnose from './models/diagnoses'
-import definePatient from './models/patients'
-import { forEach} from "lodash";
+import { forEach } from "lodash";
+import definePatient from "./models/patient_models";
+import defineDiagnose from "./models/diagnose_model";
+import defineSubstance from "./models/substance_models";
+import defineUser from "./models/user_model";
 
 const env = process.env.NODE_ENV
 const { url, options } = (database as any)[env]
-const sequelize = new Sequelize(url, options)
 
-sequelize.authenticate()
-    .then(() => console.log('Database connection has been established succesfully'))
-    .catch((err) => console.log('Unable to connect to database ${err.messages}'))
+const sequelize = new Sequelize( url, options )
+
+sequelize
+    .authenticate()
+    .then(() => console.log('Database connection has been established successfully'))
+    .catch((err) => console.log(`Unable to coonect to database ${err.messages}`))
+
 
 const modelsBuilder = (instance: Sequelize) => ({
     Substance: defineSubstance(instance, 'substance'),
     Diagnose: defineDiagnose(instance, 'diagnose'),
-    Patient: definePatient(instance, 'patient')
+    Patient: definePatient(instance, 'patient'),
+    User: defineUser(instance, 'user')
 })
+
 
 const buildModels = () => {
     const models = modelsBuilder(sequelize)
 
     forEach(models, (model: any) => {
-        if(model.associate){
+        if (model.associate) {
             model.associate(models)
         }
     })
+
     return models
 }
 
