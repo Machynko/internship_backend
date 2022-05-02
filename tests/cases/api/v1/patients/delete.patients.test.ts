69a73b3c95c5af7cf5d4a050535bc9d551560a79
Machynko <97360562+Machynko@users.noreply.github.com>
2022-05-02 19:22:37 +0200

    Create delete.patients.test.ts

---
+++ tests/cases/api/v1/patients/delete.patients.test.ts
@@ -0,0 +1,49 @@
+import supertest from "supertest";
+import { expect} from "chai";
+import app from '../../../../../src/app'
+import {schema} from "../../../../../src/api/v1/patients/delete.patients";
+import {response_schema} from "../../../../../src/api/v1/patients/get.patients";
+
+const url = '/api/v1/patients/'
+const patientID: number = 23
+
+describe(`[DELETE] ${url + `${patientID}`}`, () => {
+    it('Response should return message about deleted patient', async() => {
+        const response = await supertest(app)
+            .delete(url + patientID)
+            .set('Content-Type', 'application/json')
+
+        expect(response.status).to.eq(200)
+        expect(response.type).to.eq('application/json')
+
+        const validationResult = response_schema.validate(response.body)
+        expect(validationResult.error).to.eq(undefined)
+
+
+    })
+
+    it('Response should return 404 because patient should be deleted', async() =>{
+        const response = await supertest(app)
+            .get(url+patientID)
+            .set('Content-Type', 'application/json')
+
+        expect(response.status).to.eq(404)
+        expect(response.type).to.eq('application/json')
+
+        const validationResult = schema.validate(response.body)
+        expect(validationResult.error).to.eq(undefined)
+    })
+    it('Response should delete patient (404) but didnt find ID', async() => {
+        const response = await supertest(app)
+            .delete(url + 69420)
+            .set('Content-Type', 'application/json')
+
+        expect(response.status).to.eq(404)
+        expect(response.type).to.eq('application/json')
+
+        const validationResult = schema.validate(response.body)
+        expect(validationResult.error).to.eq(undefined)
+          
+
+    })
+})
