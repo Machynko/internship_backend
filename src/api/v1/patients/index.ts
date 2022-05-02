90627dd2dc93613effe82f2102c8a593179fcf40
Machynko <97360562+Machynko@users.noreply.github.com>
2022-05-02 20:06:36 +0200

    update of permissions

--- src/api/v1/patients/index.ts
+++ src/api/v1/patients/index.ts
@@ -1,18 +1,37 @@
-import { Router } from 'express'
+import {Router} from 'express'
 
-import validationMiddleware from '../../../middlewares/validationMiddleware'
-
+import validationMiddleware from "../../../middlewares/validationMiddleware";
 import * as GetPatients from './get.patients'
-import * as PostPatient from './post.patient'
-import * as PatchPatient from './patch.patients'
-import * as DeletePatient from './delete.patients'
+import * as PostPatients from './post.patients'
+import * as DeletePatients from './delete.patients'
+import * as PatchPatients from './patch.patients'
+import passport from "passport";
+import permissionMiddleware from "../../../middlewares/permissionMiddleware";
+import {UserRole} from "../../../enums/enums";
 
 const router = Router()
 
 export default () => {
-  router.get('/', GetPatients.workflow)
-  router.post('/', validationMiddleware(PostPatient.schema), PostPatient.workflow)
-  router.patch('/:id', validationMiddleware(PatchPatient.schema), PatchPatient.workflow)
-  router.delete('/:id', DeletePatient.workflow)
-  return router
+    router.get('/',
+        passport.authenticate('jwt-api'),
+        permissionMiddleware([UserRole.ADMIN]),
+        validationMiddleware(GetPatients.patient_schema),
+        GetPatients.workflow)
+    router.post('/',
+        passport.authenticate('jwt-api'),
+        permissionMiddleware([UserRole.USER, UserRole.ADMIN, UserRole.SUPER_ADMIN]),
+        validationMiddleware(PostPatients.schema),
+        PostPatients.workflow)
+    router.patch('/:patientId',
+        passport.authenticate('jwp-api'),
+        permissionMiddleware([UserRole.SUPER_ADMIN]),
+        validationMiddleware(PatchPatients.schema),
+        PatchPatients.workflow)
+    router.delete('/:patientId', 
+        passport.authenticate('jwt-api'),
+        permissionMiddleware([UserRole.SUPER_ADMIN]),
+        validationMiddleware(DeletePatients.schema),
+        DeletePatients.workflow)
+    
+    return router
 }
